61cd27369048c675d7cfabc4c06eed797e0f7aa4
adeyinkajoseph <adeyinkajoseph754@gmail.com>
2019-06-30 16:31:39 +0100

    add port

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.send('welcome to propertypro-lite');
 });
-app.listen(3000, () => console.log('server started on port 3000'));
+
+
+app.get('/home', (req, res) => {
+  res.send('this is my homepage');
+});
+app.listen(PORT, () => console.log('server started on port 3000'));
