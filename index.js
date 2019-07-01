3febc00177d98044b7f0dc453fc812e3ba59cada
adeyinkajoseph <adeyinkajoseph754@gmail.com>
2019-07-01 22:50:32 +0100

    setup mocha test

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('welcome to propertypro-lite');
+  res.status(200).json({
+    message: 'welcome to propertypro-lite',
+  });
 });
 
 
 app.get('/home', (req, res) => {
   res.send('this is my homepage');
 });
-app.listen(PORT, () => console.log('server started on port 3000'));
+export default app.listen(PORT, () => console.log('server started on port 3000'));
